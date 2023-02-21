24808be508132986574e3fd094f84f06aabc4d85
Ashish <enigmaticwolf83@gmail.com>
2023-02-21 16:11:07 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const server = http.createServer((req,res)=>{
+    if(req.url === '/'){
+        res.write('hello world');
+        res.end()
+    }
+    else if (req.method=== 'GET' && req.url === '/api'){
+        res.write('Hellooooo.......');
+        res.end();
+    }
+    else if(req.method === 'GET' && req.url === '/contactus'){
+        res.write('abcdddd...........');
+        // res.render('<h1>THis is h1 tag</h1>')
+        res.end();
+    }
+})
+
+server.listen(3000)
